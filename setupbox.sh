f65bf4d403c3ebcf9ad5d2432932f3c23a39e783
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 18:04:48 -0500

    setup install couchbase

--- setupbox.sh
+++ setupbox.sh
@@ -27,5 +27,11 @@ sudo apt-get -y install cgroup-bin
 sudo tar -xzvf google-cloud-sdk-169.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 
+#install couchbase
+curl -O http://packages.couchbase.com/releases/couchbase-release/couchbase-release-1.0-3-amd64.deb 
+sudo dpkg -i couchbase-release-1.0-3-amd64.deb
+#sudo apt-get update
+sudo apt-get install couchbase-server-community
+
 #run couchbase
 #sudo docker run -d -p 8091-8093:8091-8093 -p 11210:11210 couchbase
