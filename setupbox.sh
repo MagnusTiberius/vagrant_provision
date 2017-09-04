d019631193146279eae35c04275b15688fc2066d
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 15:56:57 -0500

    edit

--- setupbox.sh
+++ setupbox.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
 #install golang
-#sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
+sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
 sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
 #install git
 sudo apt-get -y install git
-
-#install gRPC
-#go get -u github.com/golang/protobuf/proto
-#go get -u github.com/golang/protobuf/protoc-gen-go
-#go get -u google.golang.org/grpc
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
@@ -24,14 +19,10 @@ sudo cp docker/* /usr/bin/
 #fix: ref-> https://stackoverflow.com/questions/24396071/docker-error-mountpoint-not-found
 sudo apt-get -y install cgroup-bin
 
-sudo dockerd &
-
-#test
-sudo docker run hello-world
 
 
 #install google cloud sdk
-#wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-169.0.0-linux-x86_64.tar.gz >/dev/null 2>&1
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-169.0.0-linux-x86_64.tar.gz >/dev/null 2>&1
 sudo tar -xzvf google-cloud-sdk-169.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 
