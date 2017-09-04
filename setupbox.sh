2c894b9ce011a3bb0f1ca9eb752e7ddbfaabed5b
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 13:40:24 -0500

    edit

--- setupbox.sh
+++ setupbox.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+#install golang
 sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
+#install git
 sudo apt-get install git
 
+#install gRPC
 go get -u github.com/golang/protobuf/proto
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u google.golang.org/grpc
