9d891737139f28d5f90c36db995bbe8cadd39d6b
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 13:26:28 -0500

    edit

--- setupbox.sh
+++ setupbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
-sudo tar -zxvf go1.9.linux-amd64.tar.gz.1
+wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
 sudo apt-get install git
