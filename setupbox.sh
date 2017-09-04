1a1aa136cd5b4539f12efd2c2288bb8329762d30
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 13:38:30 -0500

    install docker

--- setupbox.sh
+++ setupbox.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
+sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz
 sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
@@ -10,3 +10,9 @@ go get -u github.com/golang/protobuf/proto
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u google.golang.org/grpc
 
+
+#install docker
+sudo wget https://download.docker.com/linux/static/stable/x86_64/docker-17.03.0-ce.tgz
+sudo tar -xzvf docker-17.03.0-ce.tgz
+sudo cp docker/* /usr/bin/
+sudo dockerd &
