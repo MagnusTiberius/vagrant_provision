05999b976593d2df92f69794508b04d580cc0564
Ben Gonzales <bgonzales20001@gmail.com>
2017-09-04 23:39:26 -0500

    edit

--- setupbox.sh
+++ setupbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #install golang
-#sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
+sudo wget https://storage.googleapis.com/golang/go1.9.linux-amd64.tar.gz >/dev/null 2>&1
 sudo tar -zxvf go1.9.linux-amd64.tar.gz
 sudo mv go /usr/local
 
@@ -12,7 +12,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 #install docker
-#sudo wget https://download.docker.com/linux/static/stable/x86_64/docker-17.03.0-ce.tgz >/dev/null 2>&1
+sudo wget https://download.docker.com/linux/static/stable/x86_64/docker-17.03.0-ce.tgz >/dev/null 2>&1
 sudo tar -xzvf docker-17.03.0-ce.tgz
 sudo cp docker/* /usr/bin/
 
@@ -23,7 +23,7 @@ sudo apt-get -y install cgroup-bin
 #sudo dockerd &
 
 #install google cloud sdk
-#wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-169.0.0-linux-x86_64.tar.gz >/dev/null 2>&1
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-169.0.0-linux-x86_64.tar.gz >/dev/null 2>&1
 sudo tar -xzvf google-cloud-sdk-169.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
 
